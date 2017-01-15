96d97b3a8994b190ef3e6d8d72f8143d0885a28f
xMcCraftDEV <xmccraft.team@gmail.com>
2017-01-15 16:36:34 +0100

    Create ItemCreator.java

---
+++ src/CraftAPI.BedWars.APIs/ItemCreator.java
@@ -0,0 +1,37 @@
+package CraftAPI.BedWars.APIs;
+
+import java.util.ArrayList;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+/** 
+* @author xMcCraft
+*/
+
+public class ItemCreator {
+	
+	public static ItemStack CreateItemwithID(int id,int subid,int amount , String DisplayName,ArrayList<String> lore) {
+		
+		ItemStack is = new ItemStack(id,amount,(short)subid);
+		ItemMeta im = is.getItemMeta();
+		im.setDisplayName(DisplayName);
+		im.setLore(lore);
+		is.setItemMeta(im);
+		return is;
+	}
+	
+	public static ItemStack CreateItemwithMaterial(Material m,int subid,int amount , String DisplayName,ArrayList<String> lore) {
+		
+		ItemStack is = new ItemStack(m,amount,(short)subid);
+		ItemMeta im = is.getItemMeta();
+		im.setDisplayName(DisplayName);
+		im.setLore(lore);
+		is.setItemMeta(im);
+		return is;
+	}
+	
+	
+	
+}
